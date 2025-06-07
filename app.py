4ccd64bd7fdc5591019fdad3ef21480b0954c352
dkwang62 <korealocks@gmail.com>
2025-06-07 12:43:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -418,6 +418,8 @@ resort_aliases = {
     "The Westin Ka'anapali Ocean Resort Villas": "The Westin Ka'anapali Ocean Resort Villas",
     "Playa Andaluza": "Playa Andaluza",
     "Marbella Beach": "Marbella Beach",
+    "Marriott's Beachplace Towers",
+    "Sheraton Kauai Resort",
     "Village Paris": "Village Paris"
 }
 reverse_aliases = {v: k for k, v in resort_aliases.items()}
