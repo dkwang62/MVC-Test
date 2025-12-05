73e5181bbf80cd76088acbffa0ac5b36d8e31434
dkwang62 <korealocks@gmail.com>
2025-12-06 05:36:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ if checkin != checkin_input:
 # Rent rate with hint
 rate = st.number_input(
     "MVC Abound maintenance rate",
-    0.30, 1.50, default_rate, 0.83, format="%.3f"
+    0.30, 1.50, default_rate, 0.83, format="%.2f"
 )
 
 discount_display = st.selectbox(
